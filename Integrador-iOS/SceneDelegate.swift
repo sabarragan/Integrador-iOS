ba57c8a4ee01048bb3fed2fcefe44fb50edeb272
Jacobo Corvalán <jacobo.corvalan@mercadolibre.com>
2022-09-07 13:49:57 -0300

    mensaje

--- Integrador-iOS/SceneDelegate.swift
+++ Integrador-iOS/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let homeViewController = MainViewController()
+        let homeViewController = ScreenStartViewController()
         let navController = UINavigationController(rootViewController: homeViewController)
         window?.rootViewController = navController
         window?.makeKeyAndVisible()
